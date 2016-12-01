df8e7cc3d61663202523419b9c21077b0491ce1f
Raghu Betina <raghu@firstdraft.com>
2016-12-01 04:07:19 +0000

    Integrate Ransack

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,7 @@
 class RestaurantsController < ApplicationController
   def index
-    @restaurants = Restaurant.page(params[:page]).per(10)
+    @q = Restaurant.ransack(params[:q])
+    @restaurants = @q.result(:distinct => true).includes(:reviews).page(params[:page]).per(10)
     @location_hash = Gmaps4rails.build_markers(@restaurants.where.not(:location_latitude => nil)) do |restaurant, marker|
       marker.lat restaurant.location_latitude
       marker.lng restaurant.location_longitude
